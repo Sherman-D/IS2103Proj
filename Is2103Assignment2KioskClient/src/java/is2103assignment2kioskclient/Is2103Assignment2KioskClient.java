d6d0f3315c949b91e6a52eda910750186663085d
Elaine Tan <elainet@comp.nus.edu.sg>
2020-04-11 14:10:32 +0800

    Added SelfServiceKioskOperationModule & edited KioskClient

--- Is2103Assignment2KioskClient/src/java/is2103assignment2kioskclient/Is2103Assignment2KioskClient.java
+++ Is2103Assignment2KioskClient/src/java/is2103assignment2kioskclient/Is2103Assignment2KioskClient.java
@@ -1,10 +1,27 @@
 package is2103assignment2kioskclient;
 
+import ejb.session.stateless.AppointmentEntitySessionBeanRemote;
+import ejb.session.stateless.DoctorEntitySessionBeanRemote;
+import ejb.session.stateless.PatientEntitySessionBeanRemote;
+import ejb.session.stateless.StaffEntitySessionBeanRemote;
+import javax.ejb.EJB;
+
+
 public class Is2103Assignment2KioskClient {
+    
+    
+    @EJB
+    private static AppointmentEntitySessionBeanRemote appointmentEntitySessionBeanRemote;
+    @EJB
+    private static DoctorEntitySessionBeanRemote doctorEntitySessionBeanRemote;
+    @EJB
+    private static PatientEntitySessionBeanRemote patientEntitySessionBeanRemote;
+    @EJB
+    private static StaffEntitySessionBeanRemote staffEntitySessionBeanRemote; 
 
     
     public static void main(String[] args) {
-        MainApp mainApp = new MainApp();
+        MainApp mainApp = new MainApp(appointmentEntitySessionBeanRemote, doctorEntitySessionBeanRemote, patientEntitySessionBeanRemote, staffEntitySessionBeanRemote);
         mainApp.runApp();   
     }
     
